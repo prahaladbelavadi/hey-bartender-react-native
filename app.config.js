fbd8842da1cc60f9c018a5da9827a464e80d6ccb
Prahalad Belavadi <github@belavadi.com>
2021-06-07 13:35:27 +0530

    add splash screen and change version according to build; Add a demo video also

--- app.config.js
+++ app.config.js
@@ -3,13 +3,13 @@ export default {
     "./assets/logo.png",
   splash: {
     image:
-    "./assets/splash-screen.png",
+      "./assets/splash-screen.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
   },
   "android": {
     "package": "com.prahaladbelavad.heybartender",
     "permissions": [],
+    "versionCode": 2
   },
-
 };
